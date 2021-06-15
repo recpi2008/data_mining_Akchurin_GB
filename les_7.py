bd12e82b2af351ea58978569e1705b8b4c7e83e0
ivane <ivaneleskin2019@gmail.com>
2021-06-15 12:45:08 +0300

    Les_7

---
+++ les_7.py
@@ -0,0 +1,13 @@
+# скачивание файла (минусы кидает в оперативку пока скачивает)
+# import requests
+#
+url = 'https://www.karusel-tv.ru/media/suit/preview_full/media/image/2019/05/1558107489833213_1.png'
+# response = requests.get(url)
+#
+# with open('bob.png', 'wb') as f:
+#     f.write(response.content)
+
+# скачивание файла через wget (по работе похож на браузер, делит файл на куски, дает имя файла)
+# import wget
+# wget.download(url)
+
